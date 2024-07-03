6b219937f0da7b27ec2f637c94e93f32fe5f4e27
sidd <n2verz@gmail.com>
2024-07-03 12:38:08 -0400

    initial

---
+++ src/main/java/com/example/stackworkflow/WorkStack.java
@@ -0,0 +1,19 @@
+package com.example.stackworkflow;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class WorkStack {
+    // Stack of strings that hold the task's name
+    Deque<String> stack = new ArrayDeque<String>();
+    // Number of tasks visible in front of the user -- number of blocks user will see
+    private int visibleTasks = 1;
+
+    public void add(String taskName) {
+        stack.add(taskName);
+    }
+
+    public void delete() {
+        stack.clear();
+    }
+}
