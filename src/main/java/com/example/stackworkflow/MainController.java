dbef2fc55e2853dcd5ccccdede7965499852d803
sidd <n2verz@gmail.com>
2024-07-15 20:00:39 -0400

    create empty addTask function and Text Input into scene

--- src/main/java/com/example/stackworkflow/MainController.java
+++ src/main/java/com/example/stackworkflow/MainController.java
@@ -2,23 +2,32 @@ package com.example.stackworkflow;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+
+import java.util.ArrayDeque;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Deque;
 
 public class MainController
 {
     Date currentTime = new Date();
     SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
+    Deque<String> listOfTasks = new ArrayDeque<String>();
+
     @FXML
     private Label date;
 
     @FXML
     private StackPane task;
+
+    @FXML
+    private TextField input;
 
     @FXML
     public void initialize() {
@@ -30,5 +39,7 @@ public class MainController
         task.getChildren().add(exampleText);
     }
 
+    public void addTask() {
 
+    }
 }
